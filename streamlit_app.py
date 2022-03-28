3e348208c5bccb3b3927129dfddd31ab03c81efd
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-03-28 09:13:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,22 @@ import pandas as pd
 import streamlit as st
 # import plotly.express as px
 
-st.title("Test Streamlit Page :heart:")
-st.header("This is my first test workbook")
-st.write("hello world :smile:")
+st.title("Nutrition Tracker :heart:")
+#st.header("This is my first test workbook")
+st.write("Personalised Nutrition function to recommend what food to eat for the day to reach nutritional targets of: Calories, Protein,Fat, Fibre & Carbs. :smile:")
 
 ## NAME
-my_str = st.text_input('Enter name?')
-st.text(f'Hello {my_str}')
+# my_str = st.text_input('Enter name?')
+# st.text(f'Hello {my_str}')
+
+## GENDER
+sex = ['Female', 'Male', 'Non-binary']
+gender = st.selectbox("Choose Gender", sex)
+gender
 
 ## NUMBER
-number = st.slider("Enter age?", 0,100)
-number
+age = st.slider("Enter age?", 0,100)
+age
 
 ## Adding Data
 df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
