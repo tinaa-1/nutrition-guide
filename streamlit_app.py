22517293166a75f303a05ba6c67a8ac0d9848911
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-03-28 09:27:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,26 +8,27 @@ st.title("Nutrition Tracker :heart:")
 #st.header("This is my first test workbook")
 st.write("Personalised Nutrition function to recommend what food to eat for the day to reach nutritional targets of: Calories, Protein,Fat, Fibre & Carbs. :smile:")
 
-## NAME
+## Text....
 # my_str = st.text_input('Enter name?')
 # st.text(f'Hello {my_str}')
 
-## GENDER
+## SELECT GENDER
 sex = ['Female', 'Male', 'Non-binary']
-gender = st.selectbox("Choose Gender", sex)
+gender = st.selectbox("Choose Gender:", sex)
 gender
 
-## NUMBER
-age = st.slider("Enter age?", 0,100)
+## SELECT AGE
+age = st.slider("Enter age:", 0,100)
 age
+
+## SELECT GROUP
+active_levels = ['Inactive', 'Lightly Active', 'Moderately Active']
+group = st.selectbox("Choose activity level:", active_levels)
+group
 
 ## Adding Data
 df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
 cols = df.columns
-
-## SELECT BOX
-pick = st.selectbox("Choose an option", cols)
-pick
 
 # fig = px.scatter(df, x='Region', y='Sales')
 # st.plotly_chart(fig)
@@ -40,9 +41,6 @@ head = df.head()
 head
 
 
-
-"""
-### [documentation](https://docs.streamlit.io)
-### [community forums](https://discuss.streamlit.io)
-"""
+### [documentation](# https://docs.streamlit.io)
+### [community forums](# https://discuss.streamlit.io)
 
