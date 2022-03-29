aa1bf08658d0e9a981bf5576ab647512b4ca17da
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-03-29 10:10:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,37 +10,33 @@ st.write("Personalised Nutrition tracker to recommend what food to eat for the d
 
 ## NAME
 name = st.text_input('Enter name:')
-
 ## SELECT SEX
 sex = ['Female', 'Male']
 gender = st.selectbox("Select Sex:", sex)
-
 ## SELECT AGE
 age_group = ['13-18', '19-30','31-50','51+']
-age = st.selectbox("Select group:", age_group)
-
+age = st.selectbox("Select age group:", age_group)
 ## SELECT GROUP
 active_levels = ['Lightly_active', 'Moderately_active','Very_active']
-group = st.selectbox("Select:", active_levels)
-
+group = st.selectbox("Select activity level:", active_levels)
 ## SELECT DIET
-diet_pref = ['Non-veg', 'Vegetarian', 'Vegan', 'Pescatarian', 'Gluten free']
+diet_pref = ['Non_vegetarian', 'Pescatarian', 'Vegetarian', 'Vegan']
 diets = st.selectbox("Choose dietary preference:", diet_pref)
 
-st.write(f'Personalisation for {name}') 
-st.write(f'{gender}, {age}, {group}, {diets}')
-
+st.write(f'Personalisation for {name}: {gender}, {age}, {group}, {diets}')
 st.header('Recommended daily intake:')
 
 ## Adding Data
-df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
+#df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
+df2 = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQbkqBZkrJ0UInBODre8XPpk1gu-cUqZ9LCv584QoK4sPgc3GYLE2lrQHruQXprkbCg7VsoWbcHsnRr/pub?gid=2020990196&single=true&output=csv")
+head= df2.head(10)
+head
 
 # fig = px.scatter(df, x='Region', y='Sales')
 # st.plotly_chart(fig)
 # # Bar graph to show 
 # fig = px.bar(df, x=pick, y='Sales')
 # st.plotly_chart(fig)
-
 ### [documentation](# https://docs.streamlit.io)
 ### [community forums](# https://discuss.streamlit.io)
 
