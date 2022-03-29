3e5850775a17108f875f49096204165d7675b159
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-03-29 10:03:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,37 +13,33 @@ name = st.text_input('Enter name:')
 
 ## SELECT SEX
 sex = ['Female', 'Male']
-gender = st.selectbox("Choose Sex:", sex)
+gender = st.selectbox("Select Sex:", sex)
 
 ## SELECT AGE
-age = st.slider("Enter age:", 0,100)
+age_group = ['13-18', '19-30','31-50','51+']
+age = st.selectbox("Select group:", age_group)
 
 ## SELECT GROUP
-active_levels = ['Inactive', 'Lightly Active', 'Moderately Active']
-group = st.selectbox("Choose activity level:", active_levels)
+active_levels = ['Lightly_active', 'Moderately_active','Very_active']
+group = st.selectbox("Select:", active_levels)
 
 ## SELECT DIET
 diet_pref = ['Non-veg', 'Vegetarian', 'Vegan', 'Pescatarian', 'Gluten free']
 diets = st.selectbox("Choose dietary preference:", diet_pref)
 
-st.header(f'Personalisation for {name}') 
-gender, age, group, diets
+st.write(f'Personalisation for {name}') 
+st.write(f'{gender}, {age}, {group}, {diets}')
 
 st.header('Recommended daily intake:')
 
 ## Adding Data
 df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
 
-
 # fig = px.scatter(df, x='Region', y='Sales')
 # st.plotly_chart(fig)
 # # Bar graph to show 
 # fig = px.bar(df, x=pick, y='Sales')
 # st.plotly_chart(fig)
-
-head = df.head()
-head
-
 
 ### [documentation](# https://docs.streamlit.io)
 ### [community forums](# https://discuss.streamlit.io)
