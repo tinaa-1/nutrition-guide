87594df2a3e140c36d31a20150d6baa6fd37e88e
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-03-29 16:22:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def macro_calc(sex, age, activity):
   print(f'  of which Fat: {daily_fat:.0f}cals')
   
 macro_output = macro_calc(select_sex, select_age, select_activity)
-st.write(macro_output)
+st.write('test')
 
 # fig = px.scatter(df, x='Region', y='Sales')
 # st.plotly_chart(fig)
