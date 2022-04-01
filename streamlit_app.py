37709bf0b0a94fe50f94c5d0c4c13d61e4156871
tinaa-1 <101753727+tinaa-1@users.noreply.github.com>
2022-04-01 08:42:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ def food_func(sex, age, activity, diet_pref):
   return meal_plan, N, C, P, F
   
 meal_plan, N, C, P, F = food_func(select_sex, select_age, select_activity, select_diet)  
-chart_data = pd.DataFrame(meal_plan, x='Food', columns=["Carbs", "Protein","Fat"])
+chart_data = pd.DataFrame(meal_plan, columns=["Carbs", "Protein","Fat"])
               
 click2 = st.button("Recommend a meal plan")
 if click2:
